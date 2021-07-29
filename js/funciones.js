915e8472c18a886acd1dfab6c41c631049d1f60f
Gaby <mma.juandiaz@gmail.com>
2021-07-28 22:17:15 -0300

    Crea pestaña de inicio con saludo de cyborg

--- js/funciones.js
+++ js/funciones.js
@@ -1,20 +1,29 @@
+//  -----------------------------------------------------------------------------------------
+//           FUNCION PARA ABRIR Y CERRAR PESTAÑAS
+// ------------------------------------------------------------------------------------------
 function openCity(evt, cityName) {
-    // Declare all variables
+
+    // DECLARA TODAS LAS VARIABLES-------------
     var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
+    // OCULTA TODOS LOS ELEMENTOS CON CLASE= "tabcontent"------------
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
+    // OBTIENE TODOS LOS ELEMENTOS CON CLASE= "tablinks" Y ELIMINA LA CLASE "active"-----------
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Show the current tab, and add an "active" class to the link that opened the tab
+    // MUESTRA LA PESTAÑA Y AGREGA UNA CLASE "activa" AL ENLACE QUE ABRIO LA PESTAÑA----------------
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+//  -----------------------------------------------------------------------------------------
+//           DEFINE AL ELEMENTO CON EL ID "defautOpen" COMO EL PREDETERMINADO AL CARGAR LA PAGINA
+// ------------------------------------------------------------------------------------------
+document.getElementById("defaultOpen").click();
