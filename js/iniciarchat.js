ccdf25f7dbed0d5850a0fb7935b3be7c06ef5b8e
Gaby <mma.juandiaz@gmail.com>
2021-08-16 18:50:00 -0300

    Comienzo a poner funcion copy al inicio de chat

--- js/iniciarchat.js
+++ js/iniciarchat.js
@@ -27,3 +27,48 @@ function copiar2() {
     // Delete created Element       
     textArea.remove() 
 }
+
+// ----------------------------------------------
+// MINTA - ¿QUE ES MINTA?
+// ----------------------------------------------
+function copiar3() {  // Cambiar en nombre de la función
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-3').innerText  // Cambiar en nombre del Id elemnent
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// INDAGAR MEMBRESIA
+// ----------------------------------------------
+function copiar4() {  // Cambiar en nombre de la función
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-4').innerText  // Cambiar en nombre del Id elemnent
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// INDAGAR MEMBRESIA
+// ----------------------------------------------
+function copiar5() {  // Cambiar en nombre de la función
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-5').innerText  // Cambiar en nombre del Id elemnent
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
