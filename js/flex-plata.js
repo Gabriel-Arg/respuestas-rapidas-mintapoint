ebdc6cade98fe43ab9bf1da3d53628740ae15ec1
Gabriel <gabriel.diaz.dev@gmail.com>
2021-08-17 09:59:49 -0300

    Flex plata copy listo

--- js/flex-plata.js
+++ js/flex-plata.js
@@ -12,3 +12,138 @@ function copiar20() {
     // Delete created Element       
     textArea.remove() 
 }
+
+// ----------------------------------------------
+// 21 - PRECIO
+// ----------------------------------------------
+function copiar21() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-21').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 22 - PRECIO RENOVACIÓN
+// ----------------------------------------------
+function copiar22() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-22').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 23 - FORMAS DE PAGO
+// ----------------------------------------------
+function copiar23() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-23').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 24 - ES LA OPCION CORRECTA
+// ----------------------------------------------
+function copiar24() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-24').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 25 - RECOMENDAR 3
+// ----------------------------------------------
+function copiar25() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-25').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 26 - CIERRE 1
+// ----------------------------------------------
+function copiar26() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-26').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 27 - CIERRE 2
+// ----------------------------------------------
+function copiar27() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-27').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 28 - TRANSFERENCIA
+// ----------------------------------------------
+function copiar28() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-28').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 29 - CIERRE PERDIDO
+// ----------------------------------------------
+function copiar29() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-29').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
