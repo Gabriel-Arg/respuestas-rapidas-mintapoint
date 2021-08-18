6498d98d83431e993765b482f282230d5c108d7d
Gabriel <gabriel.diaz.dev@gmail.com>
2021-08-18 09:08:54 -0300

    Streaming copy listo

--- js/streaming.js
+++ js/streaming.js
@@ -12,3 +12,168 @@ function copiar157() {
     // Delete created Element       
     textArea.remove() 
 }
+
+// ----------------------------------------------
+// 158 - RECOMENDAR 2
+// ----------------------------------------------
+function copiar158() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-158').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 159 - INFORMACION
+// ----------------------------------------------
+function copiar159() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-159').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 160 - PRECIO NO MIEMBRO
+// ----------------------------------------------
+function copiar160() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-160').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 161 - PRECIO PARA MIEMBRO
+// ----------------------------------------------
+function copiar161() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-161').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 162 - ES OPCION CORRECTA
+// ----------------------------------------------
+function copiar162() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-162').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 163 - RESERVAR 1
+// ----------------------------------------------
+function copiar163() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-163').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 164 - RESERVAR 2
+// ----------------------------------------------
+function copiar164() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-164').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 165 -  RECOMENDAR 3
+// ----------------------------------------------
+function copiar165() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-165').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 166 -  CIERRE
+// ----------------------------------------------
+function copiar166() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-166').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 167 -  DATOS TRANSFERENCIA
+// ----------------------------------------------
+function copiar167() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-167').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 168 -  CIERRE PERDIDO
+// ----------------------------------------------
+function copiar168() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-168').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
