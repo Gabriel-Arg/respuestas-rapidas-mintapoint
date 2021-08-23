096635eceacd764564643c4adaf9f692381ae739
Gabriel <gabriel.diaz.dev@gmail.com>
2021-08-23 14:10:56 -0300

    Descuentos cambios

--- js/descuentos.js
+++ js/descuentos.js
@@ -1,9 +1,84 @@
 // ----------------------------------------------
-// 188 -  DERIVAR A MH4B
+// 188 -  DESCUENTO 2 MIEMBROS
 // ----------------------------------------------
 function copiar188() {       
     // Crea un input para poder copiar el texto dentro       
     let copyText = document.getElementById('rr-188').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 189 -  TRIMESTRE
+// ----------------------------------------------
+function copiar189() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-189').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 190 -  SEMESTRE
+// ----------------------------------------------
+function copiar190() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-190').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 191 -  SEMESTRE
+// ----------------------------------------------
+function copiar191() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-191').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 192 -  RECOMENDADO
+// ----------------------------------------------
+function copiar192() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-192').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
+
+// ----------------------------------------------
+// 193 -  RECOMENDADO
+// ----------------------------------------------
+function copiar193() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-193').innerText 
     const textArea = document.createElement('textarea');
     textArea.textContent = copyText;
     document.body.append(textArea);       
