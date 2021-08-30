16059b3c69b61c1e85d965edea58df06cad2c1dc
Gabriel <gabriel.diaz.dev@gmail.com>
2021-08-30 09:46:29 -0300

    Agrega nueva rr para bienvenida de CW

--- js/nuevo-cw.js
+++ js/nuevo-cw.js
@@ -27,3 +27,18 @@ function copiar182() {
     // Delete created Element       
     textArea.remove() 
 }
+
+// ----------------------------------------------
+// 182b -  BIENVENIDA
+// ----------------------------------------------
+function copiar182b() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-182b').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
