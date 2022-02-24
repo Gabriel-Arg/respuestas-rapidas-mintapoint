3e1c58ae76a1fd3edb09a38dc545ee61e3d03edd
Gaby <mma.juandiaz@gmail.com>
2022-02-24 05:51:27 -0300

    Pases 2.0 listo

--- js/day-pass.js
+++ js/day-pass.js
@@ -132,3 +132,18 @@ function copiar58() {
     // Delete created Element       
     textArea.remove() 
 }
+
+// ----------------------------------------------
+// 59 - PASE EXPRESS
+// ----------------------------------------------
+function copiar59() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-59').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
