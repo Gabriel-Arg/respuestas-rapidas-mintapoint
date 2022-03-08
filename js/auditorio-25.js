e8d4619f974759ca880edb38192b45b19cc64f32
Gaby <mma.juandiaz@gmail.com>
2022-03-08 06:43:38 -0300

    Auditorio 2022 ajustes listos

--- js/auditorio-25.js
+++ js/auditorio-25.js
@@ -192,3 +192,18 @@ function copiar130() {
     // Delete created Element       
     textArea.remove() 
 }
+
+// ----------------------------------------------
+// 130b - SITUACIÓN IDEAL DE USO
+// ----------------------------------------------
+function copiar130b() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-130b').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
