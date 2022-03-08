11b25eb134f01e832aa5f3376d6e367b67fc0500
Gaby <mma.juandiaz@gmail.com>
2022-03-08 07:38:26 -0300

    Respuestas Rápidas 2022 ajustes listos

--- js/auditorio-50.js
+++ js/auditorio-50.js
@@ -192,3 +192,18 @@ function copiar143() {
     // Delete created Element       
     textArea.remove() 
 }
+
+// ----------------------------------------------
+// 143b - SITUACIÓN IDEAL DE USO
+// ----------------------------------------------
+function copiar143b() {       
+    // Crea un input para poder copiar el texto dentro       
+    let copyText = document.getElementById('rr-143b').innerText 
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);       
+    textArea.select();       
+    document.execCommand("copy");       
+    // Delete created Element       
+    textArea.remove() 
+}
